e38e2b8d3994b3d6c3b1edd43b39b78d9f2b2768
gumball <dariltonmario14@gmail.com>
2024-05-18 18:03:00 +0100

    Mapear o tabuleiro do html para o js

---
+++ board/map.js
@@ -0,0 +1,175 @@
+const _0 = document.getElementById("s0");
+const _1 = document.getElementById("s1");
+const _2 = document.getElementById("s2");
+const _3 = document.getElementById("s3");
+const _4 = document.getElementById("s4");
+const _5 = document.getElementById("s5");
+const _6 = document.getElementById("s6");
+const _7 = document.getElementById("s7");
+const _8 = document.getElementById("s8");
+const _9 = document.getElementById("s9");
+const _10 = document.getElementById("s10");
+const _11 = document.getElementById("s11");
+const _12 = document.getElementById("s12");
+const _13 = document.getElementById("s13");
+const _14 = document.getElementById("s14");
+const _15 = document.getElementById("s15");
+const _16 = document.getElementById("s16");
+const _17 = document.getElementById("s17");
+const _18 = document.getElementById("s18");
+const _19 = document.getElementById("s19");
+const _20 = document.getElementById("s20");
+const _21 = document.getElementById("s21");
+const _22 = document.getElementById("s22");
+const _23 = document.getElementById("s23");
+const _24 = document.getElementById("s24");
+const _25 = document.getElementById("s25");
+const _26 = document.getElementById("s26");
+const _27 = document.getElementById("s27");
+const _28 = document.getElementById("s28");
+const _29 = document.getElementById("s29");
+const _30 = document.getElementById("s30");
+const _31 = document.getElementById("s31");
+const _32 = document.getElementById("s32");
+const _33 = document.getElementById("s33");
+const _34 = document.getElementById("s34");
+const _35 = document.getElementById("s35");
+const _36 = document.getElementById("s36");
+const _37 = document.getElementById("s37");
+const _38 = document.getElementById("s38");
+const _39 = document.getElementById("s39");
+const _40 = document.getElementById("s40");
+const _41 = document.getElementById("s41");
+const _42 = document.getElementById("s42");
+const _43 = document.getElementById("s43");
+const _44 = document.getElementById("s44");
+const _45 = document.getElementById("s45");
+const _46 = document.getElementById("s46");
+const _47 = document.getElementById("s47");
+const _48 = document.getElementById("s48");
+const _49 = document.getElementById("s49");
+const _50 = document.getElementById("s50");
+const _51 = document.getElementById("s51");
+const _52 = document.getElementById("s52");
+const _53 = document.getElementById("s53");
+const _54 = document.getElementById("s54");
+const _55 = document.getElementById("s55");
+const _56 = document.getElementById("s56");
+const _57 = document.getElementById("s57");
+const _58 = document.getElementById("s58");
+const _59 = document.getElementById("s59");
+const _60 = document.getElementById("s60");
+const _61 = document.getElementById("s61");
+const _62 = document.getElementById("s62");
+const _63 = document.getElementById("s63");
+const _64 = document.getElementById("s64");
+const _65 = document.getElementById("s65");
+const _66 = document.getElementById("s66");
+const _67 = document.getElementById("s67");
+const _68 = document.getElementById("s68");
+const _69 = document.getElementById("s69");
+const _70 = document.getElementById("s70");
+const _71 = document.getElementById("s71");
+const _72 = document.getElementById("s72");
+const _73 = document.getElementById("s73");
+const _74 = document.getElementById("s74");
+const _75 = document.getElementById("s75");
+const _76 = document.getElementById("s76");
+const _77 = document.getElementById("s77");
+const _78 = document.getElementById("s78");
+const _79 = document.getElementById("s79");
+
+const SQUARES = {
+    0: _0,
+    1: _1,
+    2: _2,
+    3: _3,
+    4: _4,
+    5: _5,
+    6: _6,
+    7: _7,
+    8: _8,
+    9: _9,
+    10: _10,
+    11: _11,
+    12: _12,
+    13: _13,
+    14: _14,
+    15: _15,
+    16: _16,
+    17: _17,
+    18: _18,
+    19: _19,
+    20: _20,
+    21: _21,
+    22: _22,
+    23: _23,
+    24: _24,
+    25: _25,
+    26: _26,
+    27: _27,
+    28: _28,
+    29: _29,
+    30: _30,
+    31: _31,
+    32: _32,
+    33: _33,
+    34: _34,
+    35: _35,
+    36: _36,
+    37: _37,
+    38: _38,
+    39: _39,
+    40: _40,
+    41: _41,
+    42: _42,
+    43: _43,
+    44: _44,
+    45: _45,
+    46: _46,
+    47: _47,
+    48: _48,
+    49: _49,
+    50: _50,
+    51: _51,
+    52: _52,
+    53: _53,
+    54: _54,
+    55: _55,
+    56: _56,
+    57: _57,
+    58: _58,
+    59: _59,
+    60: _60,
+    61: _61,
+    62: _62,
+    63: _63,
+    64: _64,
+    65: _65,
+    66: _66,
+    67: _67,
+    68: _68,
+    69: _69,
+    70: _70,
+    71: _71,
+    72: _72,
+    73: _73,
+    74: _74,
+    75: _75,
+    76: _76,
+    77: _77,
+    78: _78,
+    79: _79,
+}
+
+const PP1_HOUSE = document.querySelectorAll(".pp1_house");
+const PP2_HOUSE = document.querySelectorAll(".pp2_house");
+const PP3_HOUSE = document.querySelectorAll(".pp3_house");
+const PP4_HOUSE = document.querySelectorAll(".pp4_house");
+
+const PP1_START = 0;
+const PP2_START = 20;
+const PP3_START = 60;
+const PP4_START = 40;
+
+export {SQUARES, PP1_HOUSE, PP2_HOUSE, PP3_HOUSE, PP4_HOUSE, PP1_START, PP3_START, PP2_START, PP4_START}
